d4a6535acdc35e0275714e9251a9a7fee7404518
Xavier Truong <truong.xavier@gmail.com>
2024-03-23 11:32:43 +0100

    BAH-34 Add AI integration and API calls for generating text and images + refactorisation

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -1,168 +1,119 @@
-require 'json'
-require 'net/http'
-require "open-uri"
-require 'uri'
-require 'rest_client'
+require 'open-uri'
 
 class PromptsController < ApplicationController
-  attr_accessor :list_ia, :choix
-
   def create
-
-    if params[:prompt][:non_model_field][:choix] == "texte"
-      params[:prompt][:non_model_field][:list_ia].each do |ianame|
-        if ianame != ""
-          @node = Node.find(params[:node_id])
-          @prompt = Prompt.new(prompt_params)
-          @prompt.node = @node
-          @prompt.ai_class = AiClass.find_by(name: ianame)
-          if @node.title == ""
-            @node.title = params[:prompt][:prompt]
-            @node.save
-          end
-          if ianame == "ChatGPT"
-            @prompt.response_text = call_chatgpt_result_text(@prompt)
-          else
-            @prompt.response_text = call_mistralai_result_text(@prompt)
-          end
-          if @prompt.save!
-            NodeChannel.broadcast_to(
-              @node,
-              render_to_string(partial: "prompt", locals: {prompt: @prompt})
-            )
-            head :ok
-          else
-            render "nodes/show", status: :unprocessable_entity
-          end
-        end
-      end
+    list_ia = params[:prompt][:non_model_field][:list_ia]
+    choix = params[:prompt][:non_model_field][:choix]
+    prompt_text = params[:prompt][:prompt]
+    @node = Node.find(params[:node_id])
+    if choix == "texte"
+      traite_text(list_ia, prompt_text)
     else
-      params[:prompt][:non_model_field][:list_ia].each do |ianame|
-        if ianame != ""
-          @node = Node.find(params[:node_id])
-          @prompt = Prompt.new(prompt_params)
-          @prompt.node = @node
-          @prompt.ai_class = AiClass.find_by(name: ianame)
-          if @node.title == ""
-            @node.title = params[:prompt][:prompt]
-            @node.save
-          end
-          if ianame == "ChatGPT"
-            file = URI.open(call_chatgpt_result_image(@prompt))
-            @prompt.response_image.attach(io: file, filename: "#{params[:prompt][:prompt].gsub(' ','')}.png", content_type: "image/png")
-            @prompt.save
-          elseif ianame == "Dezgo"
-            data_image = call_dezgo_result_image(@prompt)
-            @prompt.response_image.attach(io: StringIO.new(data_image), filename: 'image.jpg', content_type: 'image/jpeg')
-          else
-            file = URI.open(call_deepai_result_image(@prompt))
-            @prompt.response_image.attach(io: file, filename: "#{params[:prompt][:prompt].gsub(' ','')}.png", content_type: "image/png")
-            @prompt.save
-          end
-          if @prompt.save!
-            NodeChannel.broadcast_to(
-              @node,
-              render_to_string(partial: "prompt", locals: {prompt: @prompt})
-            )
-            head :ok
-          else
-            render "nodes/show", status: :unprocessable_entity
-          end
-        end
-      end
+      traite_image(list_ia, prompt_text)
     end
   end
 
   private
 
-  def call_chatgpt_result_text(prompt)
-    client = OpenAI::Client.new
-    chaptgpt_response = client.chat(parameters: {
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: prompt.prompt.to_s }]
-    })
-    chaptgpt_response["choices"][0]["message"]["content"]
-  end
+  def traite_text(list_ia, prompt_text)
+    list_ia.each do |name_ai|
+      next if name_ai == ""
 
-  def call_chatgpt_result_image(prompt)
-    client = OpenAI::Client.new
-    response = client.images.generate(parameters: {
-      model: "dall-e-3",
-      prompt: prompt.prompt.to_s
-    })
-    response.dig("data", 0, "url")
-  end
-
-  def call_deepai_result_image(prompt)
-    result = RestClient::Request.execute(method: :post, url: 'https://api.deepai.org/api/text2img', timeout: 600,
-      headers: {
-        'Content-Type': 'application/json',
-        'api-key' => ENV['DEEPAI_API_KEY']},
-      payload: {
-          'text' => prompt.prompt,
-      })
-    response_json = JSON.parse(result.body)
-    response_json["output_url"]
-  end
-
-  def call_mistralai_result_text(prompt)
-    # Votre clé API Dezgo
-    api_key = ENV['MISTRALAI_API_KEY']
-    # Créer l'URI pour la requête API
-    uri = URI("https://api.mistral.ai/v1/chat/completions")
-
-    # Créer la requête HTTP
-    request = Net::HTTP::Post.new(uri)
-    request['Content-Type'] = 'application/json'
-    request['Accept'] = 'application/json'
-    request['Authorization'] = "Bearer #{api_key}"
-    request.body = {
-      model: 'mistral-tiny',
-      messages: [{ role: 'user', content: prompt.prompt.to_s }]
-    }.to_json
-
-    # Envoyer la requête
-    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
-      http.request(request)
-    end
-
-    if response.code.to_i == 200
-      # Analyser la réponse JSON
-      response_json = JSON.parse(response.body)
-
-      # Extraire le contenu du message
-      response_json.dig("choices", 0, "message", "content")
-    else
-      puts "Erreur lors de l'appel à l'API : #{response.code} - #{response.message}"
+      prepare_prompt(name_ai)
+      node_title_save(prompt_text)
+      prepare_response_text(prompt_text, name_ai)
+      prompt_save_and_broadcast
     end
   end
 
-  def call_dezgo_result_image(prompt)
-    # Le modèle que vous voulez utiliser
-    model = 'realistic_vision_5_1'
+  def traite_image(list_ia, prompt_text)
+    list_ia.each do |name_ai|
+      next if name_ai == ""
 
-    # Créer l'URI pour la requête API
-    uri = URI("https://api.dezgo.com/text2image_sdxl")
-
-    # Créer la requête HTTP
-    request = Net::HTTP::Post.new(uri)
-    request['X-Dezgo-Key'] = ENV.fetch('DEZGO_API_KEY')
-    request['Content-Type'] = 'application/json'
-    request.body = { prompt: prompt.prompt }.to_json
-
-    # Envoyer la requête
-    response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-      http.request(request)
-    end
-    # Vérifier le statut de la réponse
-    if response.code == '200'
-      response.body
-    else
-      puts "Erreur lors de la création de l'image: #{response.body}"
+      prepare_prompt(name_ai)
+      node_title_save(prompt_text)
+      prepare_response_image(prompt_text, name_ai)
+      prompt_save_and_broadcast
     end
   end
 
   def prompt_params
     params.require(:prompt).permit(:prompt)
+  end
+
+  def prepare_prompt(name_ai)
+    @prompt = Prompt.new(prompt_params)
+    @prompt.node = @node
+    @prompt.ai_class = AiClass.find_by(name: name_ai)
+  end
+
+  def prompt_save_and_broadcast
+    if @prompt.save!
+      NodeChannel.broadcast_to(
+        @node,
+        render_to_string(partial: "prompt", locals: { prompt: @prompt })
+      )
+      head :ok
+    else
+      render "nodes/show", status: :unprocessable_entity
+    end
+  end
+
+  def node_title_save(prompt_text)
+    return unless @node.title == ""
+
+    @node.title = prompt_text
+    @node.save
+  end
+
+  def prepare_response_text(prompt_text, name_ai)
+    case name_ai
+    when "ChatGPT"
+      @prompt.response_text = call_chatgpt_result_text(prompt_text)
+    when "MistralAi"
+      @prompt.response_text = call_mistralai_result_text(prompt_text)
+    end
+  end
+
+  def prepare_response_image(prompt_text, name_ai)
+    case name_ai
+    when "ChatGPT"
+      prepare_image_for_chatgpt(prompt_text)
+    when "Dezgo"
+      prepare_image_for_dezgo(prompt_text)
+    when "DeepAi"
+      prepare_image_for_deepai(prompt_text)
+    end
+  end
+
+  def garder_les_n_mots(chaine, number)
+    chaine.split.take(number).join(' ')
+  end
+
+  def prepare_image_for_chatgpt(prompt_text)
+    file = URI.open(call_chatgpt_result_image(prompt_text))
+    @prompt.response_image.attach(
+      io: file,
+      filename: "#{garder_les_n_mots(prompt_text, 10).gsub(' ', '')}.png",
+      content_type: "image/png"
+    )
+  end
+
+  def prepare_image_for_dezgo(prompt_text)
+    data_image = call_dezgo_result_image(prompt_text)
+    @prompt.response_image.attach(
+      io: StringIO.new(data_image),
+      filename: "#{garder_les_n_mots(prompt_text, 10).gsub(' ', '')}.jpg",
+      content_type: 'image/jpeg'
+    )
+  end
+
+  def prepare_image_for_deepai(prompt_text)
+    file = URI.open(call_deepai_result_image(prompt_text))
+    @prompt.response_image.attach(
+      io: file,
+      filename: "#{garder_les_n_mots(prompt_text, 10).gsub(' ', '')}.png",
+      content_type: "image/png"
+    )
   end
 end
--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,5 +1,113 @@
+require 'json'
+require 'net/http'
+require 'uri'
+require 'rest_client'
+require 'openai'
+
 class Prompt < ApplicationRecord
   belongs_to :node
   belongs_to :ai_class
   has_one_attached :response_image
+end
+
+def call_chatgpt_result_text(prompt)
+  client = OpenAI::Client.new
+  chaptgpt_response = client.chat(
+    parameters: {
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }]
+    }
+  )
+  chaptgpt_response["choices"][0]["message"]["content"]
+end
+
+def call_chatgpt_result_image(prompt)
+  client = OpenAI::Client.new
+  response = client.images.generate(
+    parameters: {
+      model: "dall-e-3",
+      prompt: prompt
+    }
+  )
+  response.dig("data", 0, "url")
+end
+
+def call_deepai_result_image(prompt)
+  result = execute_deepai_request(prompt)
+  response_json = JSON.parse(result.body)
+  response_json["output_url"]
+end
+
+def call_mistralai_result_text(prompt)
+  request = build_mistralai_request(prompt)
+  send_mistralai_request(request)
+end
+
+def call_dezgo_result_image(prompt)
+  request = build_dezgo_request(prompt)
+  send_dezgo_request(request)
+end
+
+private
+
+def build_dezgo_request(prompt)
+  request = Net::HTTP::Post.new(URI("https://api.dezgo.com/text2image_sdxl"))
+  request['X-Dezgo-Key'] = ENV.fetch('DEZGO_API_KEY')
+  request['Content-Type'] = 'application/json'
+  request.body = { prompt: prompt }.to_json
+  request
+end
+
+def send_dezgo_request(request)
+  uri = URI("https://api.dezgo.com/text2image_sdxl")
+  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+    http.request(request)
+  end
+  # Vérifier le statut de la réponse
+  if response.code == '200'
+    response.body
+  else
+    puts "Erreur lors de la création de l'image: #{response.body}"
+  end
+end
+
+def build_mistralai_request(prompt)
+  uri = URI("https://api.mistral.ai/v1/chat/completions")
+  request = Net::HTTP::Post.new(uri)
+  request['Content-Type'] = 'application/json'
+  request['Accept'] = 'application/json'
+  request['Authorization'] = "Bearer #{ENV.fetch('MISTRALAI_API_KEY')}"
+  request.body = {
+    model: 'mistral-tiny',
+    messages: [{ role: 'user', content: prompt }]
+  }.to_json
+  request
+end
+
+def send_mistralai_request(request)
+  uri = URI("https://api.mistral.ai/v1/chat/completions")
+  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+    http.request(request)
+  end
+  if response.code.to_i == 200
+    # Analyser la réponse JSON
+    response_json = JSON.parse(response.body)
+    # Extraire le contenu du message
+    response_json.dig("choices", 0, "message", "content")
+  else
+    puts "Erreur lors de l'appel à l'API : #{response.code} - #{response.message}"
+  end
+end
+
+def execute_deepai_request(text)
+  RestClient::Request.execute(
+    method: :post,
+    url: 'https://api.deepai.org/api/text2img',
+    timeout: 600,
+    headers: {
+      'Content-Type': 'application/json',
+      'api-key' => ENV.fetch('DEEPAI_API_KEY')
+    },
+    payload: { 'text' => text }
+  )
 end
